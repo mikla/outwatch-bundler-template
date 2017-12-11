465353b652434a0d3612afd39992d06ec06c9397
Artsiom Miklushou <mikla.op@gmail.com>
2017-12-12 00:50:23 +0200

    Reacting on checkbox example.

--- src/main/scala/app/TutorialApp.scala
+++ src/main/scala/app/TutorialApp.scala
@@ -1,6 +1,7 @@
 package app
 
 import outwatch.dom._
+import outwatch.dom.helpers.InputEvent
 import rxscalajs.Observable
 
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
@@ -11,16 +12,26 @@ object TutorialApp {
   @JSExport
   def main(args: Array[String]): Unit = {
 
-    val seconds = Observable.interval(1000)
+    val seconds: Observable[Int] = Observable.interval(1000)
       .map(_ + 1)
       .startWith(0)
 
     val timer = div("Seconds elapsed: ", child <-- seconds)
 
+    val toggleEvents: Handler[InputEvent] = createInputHandler()
+
+    val checkboxe = div(
+      label("Hide view"),
+      input(tpe := "checkbox", change --> toggleEvents),
+      h2(hidden <-- toggleEvents.map(_.target.checked), "Visible!")
+    )
+
+
     OutWatch.render("#app",
       div(
         h1("TODO"),
-        timer
+        timer,
+        checkboxe
       )
     )
 
