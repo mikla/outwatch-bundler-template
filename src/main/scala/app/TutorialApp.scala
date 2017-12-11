ef74d9dcd63593bf4d994f286dd5ef11db3e0c82
mikla <5j49mgkT>
2017-12-11 11:28:26 +0200

    Bundling mode + timer

--- src/main/scala/app/TutorialApp.scala
+++ src/main/scala/app/TutorialApp.scala
@@ -1,15 +1,29 @@
 package app
 
 import outwatch.dom._
+import rxscalajs.Observable
 
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 
-@JSExportTopLevel("TreevioWebUI")
+@JSExportTopLevel("TutorialApp")
 object TutorialApp {
 
   @JSExport
   def main(args: Array[String]): Unit = {
-    OutWatch.render("#app", h1("TODO"))
+
+    val seconds = Observable.interval(1000)
+      .map(_ + 1)
+      .startWith(0)
+
+    val timer = div("Seconds elapsed: ", child <-- seconds)
+
+    OutWatch.render("#app",
+      div(
+        h1("TODO"),
+        timer
+      )
+    )
+
   }
 
 }
